6887290542920ad62a59e7d3c0da51e65521501d
skykhs3 <skykhs3@naver.com>
2020-03-09 03:21:17 +0900

    기본틀이 되는 형태
    다른 프로젝트도 복사해서 쓰도록

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+// import Navigation from '../Navigation';
+// const App = () => (
+//   <Router>
+//     <Navigation />
+//   </Router>
+// );
+// export default App;
+import React from 'react'
+class App extends React.Component{
+    render(){
+        return(
+         <div>
+             안녕하세요. 반갑습니다.
+         </div>    
+        );
+    }
+}
+export default App;
