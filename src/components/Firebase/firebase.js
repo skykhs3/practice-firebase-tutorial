e26069af3a96c009b4d2e88cfe14a3e470a7037b
skykhs3 <skykhs3@naver.com>
2020-03-10 01:14:30 +0900

    FIREBASE IN REACT SETUP

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,14 +1,24 @@
 import app from 'firebase/app';
+// const firebaseConfig = {
+//     apiKey: process.env.apiKey,
+//     authDomain: process.env.authDomain,
+//     databaseURL: process.env.databaseURL,
+//     projectId: process.env.projectId,
+//     storageBucket: process.env.storageBucket,
+//     messagingSenderId: process.env.messagingSenderId,
+//     appId: process.env.appId,
+//     measurementId: process.env.messagingSenderId
+//   };
 const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-    measurementId: process.env.messagingSenderId
-  };
+  apiKey: "AIzaSyB2U5HcUXfL0Rg891lDHFthV29XzzSIfxw",
+  authDomain: "hyeonsu1984.firebaseapp.com",
+  databaseURL: "https://hyeonsu1984.firebaseio.com",
+  projectId: "hyeonsu1984",
+  storageBucket: "hyeonsu1984.appspot.com",
+  messagingSenderId: "30317301704",
+  appId: "1:30317301704:web:45d061c7e02278fdf63afd",
+  measurementId: "G-HRL0X3LR43"
+};
   class Firebase {
     constructor() {
       app.initializeApp(firebaseConfig);
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import App from './components/App';
-//import Firebase, { FirebaseContext } from './components/Firebase';
-ReactDOM.render(<App />, document.getElementById('root'));
+import Firebase, { FirebaseContext } from './components/Firebase';
+ReactDOM.render(<FirebaseContext.Provider value={new Firebase()}>
+    <App />
+    </FirebaseContext.Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
