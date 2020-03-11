94d89455bf3fba748e6bc84704c1cc5dfdb5281b
skykhs3 <skykhs3@naver.com>
2020-03-11 20:37:18 +0900

    Create withAuthorization.js

---
+++ src/components/Session/withAuthorization.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const withAuthorization = () => Component => {
+  class WithAuthorization extends React.Component {
+    render() {
+      return <Component {...this.props} />;
+    }
+  }
+  return WithAuthorization;
+};
+export default withAuthorization;
